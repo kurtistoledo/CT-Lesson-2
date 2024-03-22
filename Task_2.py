995cb1b0c8cb5395cdb6061f9a32f16977618b53
Kurtis Toledo <kurtistoledo@gmail.com>
2024-03-22 12:58:47 -0500

    Task_2

--- Task_2.py
+++ Task_2.py
@@ -1,8 +1,8 @@
 # 2. The Greatest Showdown
 # Task 2: Identify the Smallest
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-num3 = float(input("Enter the third number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
 
 if num1 >= num2 and num1 >= num3:
     largest = num1
