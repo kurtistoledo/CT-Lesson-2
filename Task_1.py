a263001f37ed191747b7fa18ce1889be98346ca4
Kurtis Toledo <kurtistoledo@gmail.com>
2024-03-22 12:44:55 -0500

    Task_1

---
+++ Task_1.py
@@ -0,0 +1,14 @@
+# 2. The Greatest Showdown
+# Task 1: Identify the Greatest
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
+
+print(f"The largest number among {num1}, {num2}, and {num3} is: {largest}")
