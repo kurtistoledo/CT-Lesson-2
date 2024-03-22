5a093e8547855993c94417bf3edf8f79fe041cb0
Kurtis Toledo <kurtistoledo@gmail.com>
2024-03-22 13:10:07 -0500

    Task_3

---
+++ Task_3.py
@@ -0,0 +1,27 @@
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+if num1 == num2 == num3:
+    print("All numbers are equal.")
+else:
+    if num1 >= num2 and num1 >= num3:
+        largest = num1
+    elif num2 >= num1 and num2 >= num3:
+        largest = num2
+    else:
+        largest = num3
+
+    if num1 <= num2 and num1 <= num3:
+        smallest = num1
+    elif num2 <= num1 and num2 <= num3:
+        smallest = num2
+    else:
+        smallest = num3
+
+    if num1 == num2 or num1 == num3 or num2 == num3:
+        print("Two numbers are equal.")
+
+    print(f"The largest number among {num1}, {num2}, and {num3} is: {largest}")
+    print(f"The smallest number among {num1}, {num2}, and {num3} is: {smallest}")
